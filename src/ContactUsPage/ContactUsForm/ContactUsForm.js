4aecf9deda28e38c42530c45fd093f24ae3bccbd
Denis Zinchenko <zinchenko.denis.work@gmail.com>
2020-06-10 02:33:50 +0300

    add input && clear form after send

--- src/ContactUsPage/ContactUsForm/ContactUsForm.js
+++ src/ContactUsPage/ContactUsForm/ContactUsForm.js
@@ -12,6 +12,7 @@ export const ContactUsForm = ({customClass}) => {
 
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
         .then((result) => {
+          e.target.reset();
           console.log(result.text);
         }, (error) => {
           console.log(error.text);
@@ -22,6 +23,7 @@ export const ContactUsForm = ({customClass}) => {
       <form className={customClass && customClass} onSubmit={sendEmail}>
         <input className={styles.input} type="text" name="user_name" placeholder="Name" required />
         <input className={styles.input} type="email" name="user_email" placeholder="E-mail" required />
+        <input className={styles.input} type="tel" name="user_phone" placeholder="Phone number" />
         <textarea className={styles.input} name="message" placeholder="Message" />
         <button className={styles.btn} type="submit">Send</button>
       </form>
